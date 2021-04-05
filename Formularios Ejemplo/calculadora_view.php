af38a6a16d45ccf139805c16ac36d511e3a09444
ericksitocg <ericksitocg@hotmail.com>
2021-04-05 13:19:23 -0500

    adding files

---
+++ Formularios Ejemplo/calculadora_view.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Calculadora PHP</title>
+</head>
+
+<body>
+
+<p>&nbsp;</p>
+<form name="form1" method="post" action="calculadora.php">
+  <p>
+    <label for="num1"></label>
+      <input type="text" name="num1" id="num_1">
+    <label for="num2"></label>
+      <input type="text" name="num2" id="num_2">
+    <label for="operacion"></label>
+    <select name="op" id="operacion">
+      <option value="sum">Suma</option>
+      <option value="res">Resta</option>
+      <option value="mul">Multiplicación</option>
+      <option value="div">División</option>
+      <option value="mod">Módulo</option>
+      <option value="inc">Incrementar</option>
+      <option value="dec">Decrementar</option>
+    </select>
+  </p>
+  <p>
+    <input type="submit" name="button" id="button" value="Enviar" onClick="prueba">
+  </p>
+</form>
+<p>&nbsp;</p>
+
+</body>
+</html>
